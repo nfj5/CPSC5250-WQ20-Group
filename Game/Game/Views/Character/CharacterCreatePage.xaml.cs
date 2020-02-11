686caedb2c500cee646ad3bd39f38c8b0e347d09
Nicholas Jones <jonesn6@seattleu.edu>
2020-02-11 15:02:10 -0800

    Update PersonModel to contain ImageURI

--- Game/Game/Views/Character/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Character/CharacterCreatePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,25 @@ namespace Game.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CharacterCreatePage : ContentPage
     {
-        public CharacterCreatePage()
+        GenericViewModel<CharacterModel> ViewModel { get; set; }
+
+        public CharacterCreatePage(GenericViewModel<CharacterModel> data)
         {
             InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
+            this.ViewModel.Title = "Create";
+
+            LocationPicker.SelectedItem = data.Data.Location.ToString();
+            AttributePicker.SelectedItem = data.Data.Attribute.ToString();
+        }
+        
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
+            {
+                ViewModel.Data.ImageURI = Services.CharacterService.DefaultImageURI;
+            }
         }
     }
 }

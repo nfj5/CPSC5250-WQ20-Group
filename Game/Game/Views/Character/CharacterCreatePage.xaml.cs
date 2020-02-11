4731f4da81e147e26790c335e34c5fd976273c85
Nicholas Jones <jonesn6@seattleu.edu>
2020-02-11 15:28:45 -0800

    Add basic layout for CharacterCreatePage and backend

--- Game/Game/Views/Character/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Character/CharacterCreatePage.xaml.cs
@@ -24,12 +24,60 @@ namespace Game.Views
             this.ViewModel.Title = "Create";
         }
         
+        /// <summary>
+        /// Save the new Character by calling Create
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         async void Save_Clicked(object sender, EventArgs e)
         {
             if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
             {
                 ViewModel.Data.ImageURI = Services.CharacterService.DefaultImageURI;
             }
+
+            MessagingCenter.Send(this, "Create", ViewModel.Data);
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Cancel Character creation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Capture value change for Speed stepper
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Speed_OnValueStepperChanged(object sender, ValueChangedEventArgs e)
+        {
+            SpeedValue.Text = String.Format("{0}", e.NewValue);
+        }
+
+        /// <summary>
+        /// Capture value change for Strength stepper
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Strength_OnValueStepperChanged(object sender, ValueChangedEventArgs e)
+        {
+            StrengthValue.Text = String.Format("{0}", e.NewValue);
+        }
+
+        /// <summary>
+        ///  Capture value change for HitPoints stepper
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void HitPoints_OnValueStepperChanged(object sender, ValueChangedEventArgs e)
+        {
+            HitPointsValue.Text = String.Format("{0}", e.NewValue);
         }
     }
 }

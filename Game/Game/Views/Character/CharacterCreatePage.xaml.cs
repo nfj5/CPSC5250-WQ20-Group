63ce62335c1eed42a9b8399a1c792eca131bcccc
Nicholas Jones <jonesn6@seattleu.edu>
2020-02-11 14:54:02 -0800

    Update Class from View.Character to Views

--- Game/Game/Views/Character/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Character/CharacterCreatePage.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Game.Views.Character
+namespace Game.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CharacterCreatePage : ContentPage

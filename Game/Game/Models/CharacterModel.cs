1d5a73001f6b570793c2b09c39dcfdf8651f63a4
pabi <dhaliwalpara@seattleu.edu>
2020-02-11 20:36:39 -0800

    Adding fields to character model

--- Game/Game/Models/CharacterModel.cs
+++ Game/Game/Models/CharacterModel.cs
@@ -7,5 +7,18 @@ namespace Game.Models
     // CharacterModel extends PersonModel (and is literally just uses that)
     public class CharacterModel: PersonModel
     {
+        public override void Update(CharacterModel newData)
+        {
+            if(newData == null)
+            {
+                return;
+            }
+
+            BaseSpeed
+            BaseStrength
+            BaseHitPoints
+            BaseThiccness
+        }
+
     }
 }

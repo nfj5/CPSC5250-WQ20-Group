45307010dbc907c46bc01c71e6ae061c4bf5a512
Nicholas Jones <jonesn6@seattleu.edu>
2020-02-11 16:39:28 -0800

    Added Thiccness to SuperstarAbility stat modification, set stats back to normal when Cooldown is over

--- Game/Game/Models/AbilityModel.cs
+++ Game/Game/Models/AbilityModel.cs
@@ -10,6 +10,7 @@ namespace Game.Models
         public float SpeedMultiplier { get; set; }
         public float StrengthMultiplier { get; set; }
         public float HitPointModifier { get; set; }
+        public float ThiccnessModifier { get; set; }
         public int Cooldown { get; set; }
     }
 }

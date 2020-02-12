39b891794f862f3720d48be9653b7529d12d8946
pabi <dhaliwalpara@seattleu.edu>
2020-02-11 21:29:38 -0800

    Added onappear method in cahracter index page.

--- Game/Game/Views/Character/CharacterIndexPage.xaml.cs
+++ Game/Game/Views/Character/CharacterIndexPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Game.Models;
 using Game.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,13 +13,13 @@ namespace Game.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CharacterIndexPage : ContentPage
     {
-        readonly CharacterIndexViewModel viewModel;
+        readonly CharacterIndexViewModel ViewModel;
 
         public CharacterIndexPage()
         {
             InitializeComponent();
 
-            BindingContext = viewModel = CharacterIndexViewModel.Instance;
+            BindingContext = ViewModel = CharacterIndexViewModel.Instance;
             
         }
 
@@ -27,8 +28,40 @@ namespace Game.Views
             await Navigation.PushModalAsync(new NavigationPage(new CharacterCreatePage(new ViewModels.GenericViewModel<Models.CharacterModel>())));
         }
 
-        async void OnCharacterSelected(object sender, Sel)
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            CharacterModel data = args.SelectedItem as CharacterModel;
+            if (data == null)
+            {
+                return;
+            }
 
-        
+            await Navigation.PushAsync(new CharacterReadPage(new GenericViewModel<CharacterModel>(data)));
+
+            CharactersListView.SelectedItem = null; 
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (ViewModel.Dataset.Count == 0)
+            {
+                ViewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (ViewModel.NeedsRefresh())
+            {
+                ViewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = ViewModel;
+        }
+
+
     }
 }

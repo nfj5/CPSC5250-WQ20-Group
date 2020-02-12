6dd95b57c9a3796d8c56e0488abb251bc4a4e7bc
pabi <dhaliwalpara@seattleu.edu>
2020-02-11 20:17:00 -0800

    Created insatnce in characterindexviewmodel

--- Game/Game/ViewModels/CharacterIndexViewModel.cs
+++ Game/Game/ViewModels/CharacterIndexViewModel.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+using Game.Models;
+using Game.Views;
+using Xamarin.Forms;
+using System.Linq;
+using System.Collections.Generic;
+using Game.Services;
 namespace Game.ViewModels
 {
-    public class CharacterIndexViewModel
+    //Character view Model
+    //Manages the list of data records
+    public class CharacterIndexViewModel : BaseViewModel<CharacterModel>
     {
-        public CharacterIndexViewModel()
+        #region Singleton
+
+        private static volatile CharacterIndexViewModel instance;
+        private static readonly object syncRoot = new Object();
+
+        public static CharacterIndexViewModel Instance
         {
+            get
+            {
+                if (instance == null)
+                {
+                    lock(syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new CharacterIndexViewModel();
+                            instance.Initialize();
+                        }
+                    }
+                }
+
+                return instance;
+            }
         }
+
+        #endregion Singleton
+
+        #region Constructor
+
+        //Constructor
+        //The constructor subscribes message listeners for crudi operations
+
+        
     }
 }

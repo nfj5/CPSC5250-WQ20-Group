e92061a73c6add4fb5e35034e2aca9e2e78027c7
pabi <dhaliwalpara@seattleu.edu>
2020-02-13 10:08:02 -0800

    Added Mosnte read xmal and cs page.

---
+++ Game/Game/Views/Monster/MonsterReadPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Game.Views.Monster
+{
+    public partial class MonsterReadPage : ContentPage
+    {
+        public MonsterReadPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

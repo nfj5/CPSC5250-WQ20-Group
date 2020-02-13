aec83bbf9573844f05abec007786af0e03c7cf97
Nicholas Jones <jonesn6@seattleu.edu>
2020-02-13 12:28:19 -0800

    MonsterDeletePage base

--- Game/Game/Views/Monster/MonsterDeletePage.xaml.cs
+++ Game/Game/Views/Monster/MonsterDeletePage.xaml.cs
@@ -1,15 +1,36 @@
-﻿using System;
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
-namespace Game.Views.Monster
+namespace Game.Views
 {
     public partial class MonsterDeletePage : ContentPage
     {
-        public MonsterDeletePage()
+        GenericViewModel<MonsterModel> ViewModel;
+
+        public MonsterDeletePage(GenericViewModel<MonsterModel> data)
         {
             InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
+        }
+
+        async void Delete_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "Delete", ViewModel.Data);
+            await Navigation.PopModalAsync();
+        }
+
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
         }
     }
 }

7d6d58c74317554a34989a3657e4c2d1ad75dc81
pabi <dhaliwalpara@seattleu.edu>
2020-02-13 11:34:57 -0800

    Added constructor to readpage and connected it to indexpage.

--- Game/Game/Views/Monster/MonsterIndexPage.xaml.cs
+++ Game/Game/Views/Monster/MonsterIndexPage.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Models;
 using Game.ViewModels;
 using Game.Views.Monster;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Game.Views
 {
@@ -21,6 +26,19 @@ namespace Game.Views
         {
             await Navigation.PushModalAsync(new NavigationPage(new MonsterCreatePage(new ViewModels.GenericViewModel<Models.MonsterModel>())));
             
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            MonsterModel data = args.SelectedItem as MonsterModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            await Navigation.PushAsync(new MonsterReadPage(new GenericViewModel<MonsterModel>(data)));
+
+            MonsterListView.SelectedItem = null;
         }
 
     }
--- Game/Game/Views/Monster/MonsterReadPage.xaml.cs
+++ Game/Game/Views/Monster/MonsterReadPage.xaml.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Game.ViewModels;
+using Game.Models;
 using Xamarin.Forms;
 
 namespace Game.Views.Monster
 {
     public partial class MonsterReadPage : ContentPage
     {
-        public MonsterReadPage()
+        GenericViewModel<MonsterModel> ViewModel { get; set; }
+
+        public MonsterReadPage(GenericViewModel<MonsterModel> data)
         {
             InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
+            this.ViewModel.Title = "Read";
         }
     }
 }

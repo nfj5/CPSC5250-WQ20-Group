bc0203ba43a634072859a8b649b39f72335749a0
pabi <dhaliwalpara@seattleu.edu>
2020-02-13 10:07:35 -0800

    Added monster delete xmal and cs page

---
+++ Game/Game/Views/Monster/MonsterDeletePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Game.Views.Monster
+{
+    public partial class MonsterDeletePage : ContentPage
+    {
+        public MonsterDeletePage()
+        {
+            InitializeComponent();
+        }
+    }
+}

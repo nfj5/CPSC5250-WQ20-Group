c385c68c66f24202d271396acf1485ae7fbe68b1
pabi <dhaliwalpara@seattleu.edu>
2020-02-13 10:08:32 -0800

    Added Monster Index Page

---
+++ Game/Game/Views/Monster/MonsterIndexPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Game.Views.Monster
+{
+    public partial class MonsterIndexPage : ContentPage
+    {
+        public MonsterIndexPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

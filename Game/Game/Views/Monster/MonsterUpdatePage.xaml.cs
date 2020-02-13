9e67c190d2487aa901dd48130002701131800cb1
pabi <dhaliwalpara@seattleu.edu>
2020-02-13 10:09:41 -0800

    Added monster update xmal and cs page.

---
+++ Game/Game/Views/Monster/MonsterUpdatePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Game.Views.Monster
+{
+    public partial class MonsterUpdatePage : ContentPage
+    {
+        public MonsterUpdatePage()
+        {
+            InitializeComponent();
+        }
+    }
+}

2e6894806b176640576c45d9533ae47e0a724aef
pabi <dhaliwalpara@seattleu.edu>
2020-02-13 12:34:11 -0800

    Back end for update page.

--- Game/Game/Views/Monster/MonsterUpdatePage.xaml.cs
+++ Game/Game/Views/Monster/MonsterUpdatePage.xaml.cs
@@ -1,15 +1,40 @@
-﻿using System;
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
-namespace Game.Views.Monster
+namespace Game.Views
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MonsterUpdatePage : ContentPage
     {
-        public MonsterUpdatePage()
+
+        //Change to characterviewmodel viewmodel;
+        GenericViewModel<MonsterModel> ViewModel;
+
+        public MonsterUpdatePage(GenericViewModel<MonsterModel> data)
         {
             InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
         }
+
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "Update", ViewModel.Data);
+            await Navigation.PopModalAsync();
+        }
+
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
     }
 }

54d5b4d52bb93fad233a3f087597687dfa8420b6
pabi <dhaliwalpara@seattleu.edu>
2020-02-23 15:39:11 -0800

    Added gamebaord to gamemodel.

--- Game/Game/Models/GameBoardModel.cs
+++ Game/Game/Models/GameBoardModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using Game.Models;
 namespace Game.Models
 {
     public class GameBoardModel
     {
         public GameBoardModel()
         {
+            DefaultModel[,] Gameboard = new DefaultModel[9, 9];
         }
     }
 }

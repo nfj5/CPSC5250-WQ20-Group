a82813f40c9b7bec3a2db3c90f102a565300cd57
RUEBEN-PC\Rueben T <tiowr@seattleu.edu>
2020-02-25 19:30:28 -0800

    Added comments to DataStore.cs

--- Game/Game/Models/DataStore.cs
+++ Game/Game/Models/DataStore.cs
@@ -4,8 +4,13 @@ namespace Game.Models
     // What data store will be used.  
     public enum DataStoreEnum
     {
+        // Not Specified
         Unknown = 0,
-        SQL = 1,
-        Mock = 2
+
+        // SQL Datastore
+        SQL = 10,
+
+        // Mock Datastore
+        Mock = 20
     }
 }

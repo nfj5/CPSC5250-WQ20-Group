bb07c474525ee265af8183c9f6590be665476de8
RUEBEN-PC\Rueben T <tiowr@seattleu.edu>
2020-02-25 19:13:57 -0800

    Added comments to GameBoardHelper.cs

--- Game/Game/Helpers/GameBoardHelper.cs
+++ Game/Game/Helpers/GameBoardHelper.cs
@@ -6,6 +6,14 @@ namespace Game.Helpers
 {
     class GameBoardHelper
     {
+        /// <summary>
+        /// Calculate the distance between 2 positions on the gameboard.
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        /// <returns></returns>
         public static int Distance(int x1, int y1, int x2, int y2)
         {
              return ((int) Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2)));
